e6575a054a74dc4a60afcc95d6bc60a7b10bfe17
Old King Cosmeticos <oldkingcosmeticos@gmail.com>
2026-05-28 14:19:56 -0300

    Update Omie stage to 20 (Faturar)

--- backend/src/services/OmieService.ts
+++ backend/src/services/OmieService.ts
@@ -141,7 +141,7 @@ export class OmieService {
             codigo_cliente: codigo_cliente_omie,
             codigo_pedido_integracao: order.id,
             data_previsao: new Date().toLocaleDateString('pt-BR'),
-            etapa: "50", // Etapa 50: Faturar automaticamente e gerar NF-e
+            etapa: "20", // Etapa 20: Faturar
             quantidade_itens: det.length
           },
           det: orderItems,
